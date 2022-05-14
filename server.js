fcb68c08ca800a9261fe3f6d3739dd3d5d2d8cac
InaiaKenia <inaiakenia15@gmail.com>
2022-05-14 16:01:34 -0600

    updated server.js for employee function

--- server.js
+++ server.js
@@ -135,7 +135,7 @@ function createRole() {
     },
   ]).then(data => {
     console.log("Adding role to database\n");
-    let query = `INSERT INTO role(title,salary,department_id) VALUES('${data.roleTitle}','${data.roleSalary}','${data.roleId}')`;
+    let query = `INSERT INTO roles(title,salary,department_id) VALUES('${data.roleTitle}','${data.roleSalary}','${data.roleId}')`;
     db.query(query, function (err, result) {
       if (err) throw err;
       viewAllRoles();
@@ -176,7 +176,35 @@ function createEmployee() {
 }
 
 function updateEmployeeRole() {
-  db.query("SELECT e.id, e.first_name, e.last_name, r.title, d.name AS department, r.salary, CONCAT(m.first_name, ' ', m.last_name) AS manager FROM employee e LEFT JOIN role r ON e.role_id = r.id LEFT JOIN department d ON d.id = r.department_id LEFT JOIN employee m ON m.id = e.manager_id", function (err, result) {
+  inquirer.prompt([
+{
+type:"list",
+name: "employee_id",
+ message:"select employee id",
+choices: [{name:"Cody Marin",value:1},{name:"Inaia Martin ",value:2},{name:"Fernando Mattos ",value:3}, 
+{name:"Jeane Dantas",value:4},{name:"Mari Nascimento",value:5},{name:"Ana Claudia",value:6},{name:"Agatha Smith",value:7},{name:"Cris Ripepi",value:8},{name:"Georgia Williams ",value:9},{name:"Aline Santos",value:10},
+{name:"Sangreetha K ",value:21}],
+
+},
+
+{
+  type:"list",
+  name: "role_id",
+   message:"select role id",
+  choices: [{name:" Lead Engineer ",value:1},{name:"Jr Engineer ",value:2},{name:"Marketing Director ",value:3},{name:"Lead HR ",value:4},{name:"HR Director ",value:5}, 
+  {name:"Marketing Junior  ",value:6}, {name:"Account Manager",value:7}, {name:"Account",value:8}],
+  
+  },
+
+  {
+    type:"list",
+    name: "manager_id",
+     message:"select manager id",
+    choices: [{name:"Cody Martin ",value:1},{name:" Fernando Matos ",value:3},{name:" Fernando Matos ",value:3}     ],
+    
+    },
+]).then(response => {
+  db.query("update employee set role_id=? , manager_id =? where employee_id=?;",[res.employee_id], function (err, result) {
     if (err) {
       console.log(err);
     }
@@ -185,15 +213,16 @@ function updateEmployeeRole() {
     console.table(result);
     console.log("Viewing employees to update\n");
   });
-  db.query('SELECT id, title FROM role', function (err, result) {
-    if (err) {
-      console.log(err);
-    }
-    console.log("Viewing role IDs\n");
-    console.table(result);
-    console.log("Viewing role IDs\n");
-    newRole();
-  });
+})
+  // db.query('SELECT id, title FROM role', function (err, result) {
+  //   if (err) {
+  //     console.log(err);
+  //   }
+  //   console.log("Viewing role IDs\n");
+  //   console.table(result);
+  //   console.log("Viewing role IDs\n");
+  //   newRole();
+  // });
 }
 
 function newRole() {
